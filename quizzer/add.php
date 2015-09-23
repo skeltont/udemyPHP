9aba2c62e035c6e5e15891b63e2f3bda1d736b26
Ty Skelton <skeltont@onid.oregonstate.edu>
2015-09-22 22:15:50 -0700

    adds adding a question functionality. pretty basic and I dont' like it, but whatever

--- quizzer/add.php
+++ quizzer/add.php
@@ -1,3 +1,50 @@
+<?php include 'database.php'; ?>
+<?php
+  if (isset($_POST['submit'])) {
+    // Get post variables
+    $question_number = $_POST['question_number'];
+    $question_text = $_POST['question_text'];
+    $correct_choice = $_POST['correct_choice'];
+    $choices = array(
+      $_POST['choice_1'],
+      $_POST['choice_2'],
+      $_POST['choice_3'],
+      $_POST['choice_4'],
+      $_POST['choice_5']
+    );
+
+    // Insert Question
+    $query = "INSERT INTO `questions` (text) VALUES ('$question_text')";
+    $insert_row = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+    if ($insert_row) {
+      foreach($choices as $choice => $value) {
+        if($value != '') {
+          if($correct_choice == $choice + 1) {
+            $is_correct = 1;
+          } else {
+            $is_correct = 0;
+          }
+          $query = "INSERT INTO `choices` (question_number, is_correct, text)
+                    VALUES ($question_number, $is_correct, '$value')";
+          $insert_row = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+          if($insert_row) {
+            continue;
+          } else {
+            die('Error : ('.$mysqli->errno.') '.$mysqli->error);
+          }
+        }
+      }
+      // header('Location: add.php');
+      // exit();
+      $msg = 'Question has been added';
+    }
+
+
+  }
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -14,7 +61,12 @@
     <main>
       <div class="container">
         <h2>Add A Question</h2>
-        <form action="add.php" method="post">
+        <?php
+          if(isset($msg)) {
+            echo '<p>'.$msg.'</p>';
+          }
+        ?>
+        <form action="" method="post">
           <p>
             <label>Question Number:</label>
             <input type="number" name="question_number">
@@ -48,7 +100,7 @@
             <input type="number" name="correct_choice">
           </p>
           <p>
-            <input type="submit" value="Submit">
+            <input type="submit" name="submit" value="Submit">
           </p>
 
         </form>
